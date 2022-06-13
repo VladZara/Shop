50ddf4c4a66d0feae1360bc1c3ff5dad5bf07298
VladZara <vlad.zara.2028@icloud.com>
2022-06-14 00:42:42 +0300

    topside structure

---
+++ src/components/Topside/Topside.js
@@ -0,0 +1,25 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/esm/Row';
+import Col from 'react-bootstrap/esm/Col';
+import Slider from '../Slider/Slider';
+import RightSide from '../RightSide/RightSide';
+import Info from '../Info/Info';
+
+const TopSide = () =>{
+    return (
+        <Container fluid="md">
+        <Row>
+          <Col>
+          <Slider/>
+          </Col>
+          <Col>
+          <RightSide/>
+          </Col>
+        </Row>
+        <Row >
+            <Info/>
+        </Row>    
+      </Container>
+    )
+}
+export default TopSide
