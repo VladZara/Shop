e2319952bc6fdcb6de06494638254538b893ea53
VladZara <vlad.zara.2028@icloud.com>
2022-06-16 11:10:49 +0300

    header and navbar fixed

--- src/components/Topside/Topside.js
+++ src/components/Topside/Topside.js
@@ -1,13 +1,35 @@
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/esm/Row';
-import Col from 'react-bootstrap/esm/Col';
+import {Nav, Navbar, Container, Row, Col} from 'react-bootstrap'
+import SearchIcon from '@mui/icons-material/Search';
+import PublicIcon from '@mui/icons-material/Public';
+import PersonIcon from '@mui/icons-material/Person';
+import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
 import Slider from '../Slider/Slider';
 import RightSide from '../RightSide/RightSide';
 import Info from '../Info/Info';
 
+
 const TopSide = () =>{
     return (
         <Container fluid="md" className="topside">
+           <Navbar bg="light" variant="light">
+        <Navbar.Brand>
+            <h2>Misto</h2>
+        </Navbar.Brand>
+        <div>
+            <Nav  className='head'>
+                <h5>About Us</h5>
+                <h5>Women</h5>
+                <h5>Men</h5>
+                <h5>Beauty</h5>
+                <h5>Accesories</h5>
+                <h5>Blog</h5>
+                <h5>Contact</h5>
+            </Nav>
+        </div>
+        <Nav className='ml-suto'>
+            <SearchIcon /><PublicIcon /><PersonIcon/><ShoppingBagIcon />
+        </Nav>
+  </Navbar>
         <Row>
           <Col>
           <Slider/>
