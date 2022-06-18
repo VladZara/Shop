dc5657ef92c1132a373d9bbde7395c18f0ad287a
Adelina <tapesadelina@gmail.com>
2022-06-18 12:19:43 +0300

    final-footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import TopSide from "../src/components/Topside/Topside";
 import MainNav from "./components/Header/MainNav";
-import "bootstrap/dist/css/bootstrap.min.css"; 
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      <MainNav/>
-      <TopSide/>
+        <MainNav/>
+        <TopSide/>
+        <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import  "./Footer.css"
+import FooterInfo from "./FooterInfo";
+import FooterUpperSide from "./FooterUpperSide";
+
+const Footer = () => {
+    return(
+        <div className="footer">
+            <FooterUpperSide/>
+            <FooterInfo/>
+        </div>
+    )
+}
+export default Footer
+
