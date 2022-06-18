fb3b0d2d46298e62dfa5921285f8267970d18dd9
sjnt <bragav731@gmail.com>
2022-06-18 12:09:58 +0300

    Grid is ready

---
+++ src/components/ImageGrid/Stars.js
@@ -0,0 +1,18 @@
+import star1 from "./imgs/star1.svg";
+import star2 from "./imgs/star2.svg";
+import Image from 'react-bootstrap/Image';
+import "./Cards.css"
+
+export const Stars = () => {
+    return (
+    <div className="stars">
+        <Image src={star1}></Image>
+        <Image src={star1}></Image>
+        <Image src={star1}></Image>
+        <Image src={star1}></Image>
+        <Image src={star2}></Image>
+    </div>
+    )
+}
+
+export default Stars
