4a4819737b3c926463eb9ed086d00c29069b4bc6
sjnt <bragav731@gmail.com>
2022-06-21 16:50:31 +0300

    the style of shopping cart is done

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ import ForWomen from "./components/ImageGrid/ForWomen";
 import './App.css';
 import Footer from "./components/Footer/Footer";
 import Subscribe from "./components/Subscribe/Subscribe";
+import CartSideBar from "./components/ShoppingCart/CartSideBar";
 // import WomenPage from "./components/CategoryPages/WomenPage";
 // import MenPage from "./components/CategoryPages/MenPage";
+import img from "../src/components/ImageGrid/imgs/image1.png"
+
 function App() {
   return (
     <div className="App">
+        <CartSideBar boughtItem={img} name="Hoody" colorSize="White, L" amount="2" price="$38.99" totalPrice="$78.00"/>
         <MainNav/>
         <TopSide/>
         <ForWomen/>
---
+++ src/components/ShoppingCart/CartSideBar.js
@@ -0,0 +1,24 @@
+import "./ShoppingCart.css";
+import Item from "./ItemsCreator";
+import Image from 'react-bootstrap/Image';
+import closeButton from "./icons/close.svg";
+import CartButtons from "./Buttons";
+
+
+const CartSideBar = (props) => {
+    return (
+        <div className="shopping-cart">
+            <div className="top">
+                <p className="title">SHOPPING CART</p>
+                <Image src={closeButton} className="close-button"></Image>
+            </div>
+            <Item boughtItem={props.img} name={props.name} colorSize={props.colorSize} amount={props.amount} price={props.price}/>
+            <div className="total-sum">
+                <p>Total:</p>
+                <p>{props.totalPrice}</p>
+            </div>
+            <CartButtons/>
+        </div>
+    )
+}
+export default CartSideBar
